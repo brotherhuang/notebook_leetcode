0f5ebefc86eaa990cc3811c58cb1670428b2481d
brotherhuang <jackiehuanghaibin@gmail.com>
2016-07-01 11:23:20 -0400

    add first python solution

---
+++ Reverse_String.py
@@ -0,0 +1,12 @@
+"""  Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh". """
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = s[::-1]
+        return res
