59c7435a83bea66f3b3e1c3ed91f11028f63a0f9
brotherhuang <jackiehuanghaibin@gmail.com>
2016-07-01 11:12:37 -0400

    add solution
    add solution

--- c++/Restore_IP_Addresses.cpp
+++ c++/Restore_IP_Addresses.cpp
@@ -31,3 +31,31 @@ public:
         return res;
     }
 };
+
+/*--------------------------------*/
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        for(int i = 1; i <=3; i++){
+            for(int j = 1; j <=3; j++){
+                for(int k = 1; k <=3; k++){
+                    for(int l = 1; l <=3; l++){
+                        if( i + j + k + l != s.size()) continue;
+                        string p0 = s.substr(0,i);
+                        if(stoi(p0) > 255 || (i > 1 && p0[0] == '0')) continue;
+                        string p1 = s.substr(i,j);
+                        if(stoi(p1) > 255 || (j > 1 && p1[0] == '0')) continue;
+                        string p2 = s.substr(i + j,k);
+                        if(stoi(p2) > 255 || (k > 1 && p2[0] == '0')) continue;
+                        string p3 = s.substr(i + j + k,l);
+                        if(stoi(p3) > 255 || (l > 1 && p3[0] == '0')) continue;
+                        string validIP = p0 + "." + p1 + "." + p2 + "." + p3;
+                        res.push_back(validIP);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
