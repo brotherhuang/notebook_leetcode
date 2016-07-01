570c98688b1e83ef9acb89c5bf763426aa16b017
haibin <jackiehuanghaibin@gmail.com>
2016-07-01 09:43:34 -0400

    add solution

---
+++ c++/Restore_IP_Addresses.cpp
@@ -0,0 +1,33 @@
+/* Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+For example:
+Given "25525511135",
+
+return ["255.255.11.135", "255.255.111.35"]. (Order does not matter) */
+class Solution {
+public:
+    void getValid(string s, int startPos, int currentSize, string validIP, vector<string>& res){
+        if((currentSize == 4 && startPos != s.size()) || (startPos > s.size())) return;
+        if(startPos == s.size() && currentSize == 4){
+            validIP.pop_back();
+            res.push_back(validIP);
+        }
+        for(int i = 1; i <= 3; i++){
+            if(startPos + i <= s.size()){
+                string current = s.substr(startPos, i);
+                if(i > 1 && current[0] == '0') continue;
+                if(stoi(current) <= 255) {
+                    validIP = validIP + current + '.';
+                    getValid(s,startPos + i, currentSize + 1, validIP, res);
+                    for(int  j= 0; j <= i; j++) validIP.pop_back();
+                }
+            }
+        }
+    }
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        string validIP;
+        getValid(s,0,0,validIP,res);
+        return res;
+    }
+};
