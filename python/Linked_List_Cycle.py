8c7335574ff0ab7609a9f46e2c80789cac430413
haibin <jackiehuanghaibin@gmail.com>
2016-07-03 13:14:02 -0400

    add solutions

---
+++ python/Linked_List_Cycle.py
@@ -0,0 +1,18 @@
+"""Given a linked list, determine if it has a cycle in it."""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        if head == None or head.next == None:
+            return False
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
