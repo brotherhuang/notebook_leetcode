adca6ff007b655b7720618c9bccf94349668b79c
haibin <jackiehuanghaibin@gmail.com>
2016-07-05 16:43:36 -0400

    add solutions

--- c++/Longest_Common_Prefix.cpp
+++ c++/Longest_Common_Prefix.cpp
@@ -2,14 +2,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string res;
+        string res = "";
         if(strs.size() == 0) return res;
         for(int i = 0; i < strs[0].size(); i++){
-            for(int j = 0; j < strs.size(); j++){
-                if(strs[j][i] != strs[0][i]){
-                    return res;
+            bool isCommon = true;
+            for(int j = 1; j < strs.size(); j++){
+                if(i >= strs[j].size() || strs[j][i] != strs[0][i]){
+                    isCommon = false;
+                    break;
                 }
             }
+            if(!isCommon) break;
             res.push_back(strs[0][i]);
         }
         return res;
---
+++ python/Longest_Common_Prefix.py
@@ -0,0 +1,15 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings. """
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs: return ''
+        s1 = min(strs)
+        s2 = max(strs)
+        for i, c in enumerate(s1):
+            if c != s2[i]:
+                return s1[:i]
+        return s1        
