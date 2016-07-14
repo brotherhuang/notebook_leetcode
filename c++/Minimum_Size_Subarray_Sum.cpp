42a5d837add731ea035b029da466bf8156eb57ce
haibin <hbhuang@cs.umass.edu>
2016-07-13 23:17:26 -0400

    add solutions

---
+++ c++/Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,21 @@
+/* Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint. */
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int left = 0, right = 0, sum = 0, len = nums.size(), res = len + 1;
+        while (right < len) {
+            while (sum < s && right < len) {
+                sum += nums[right++];
+            }
+            while (sum >= s) {
+                res = min(res, right - left);
+                sum -= nums[left++];
+            }
+        }
+        return res == len + 1 ? 0 : res;
+    }
+};
