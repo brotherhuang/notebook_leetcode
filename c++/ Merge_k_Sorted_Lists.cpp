c73ff550c3e6757947ccf9be46c079ca4b184e87
brotherhuang <jackiehuanghaibin@gmail.com>
2016-07-24 12:24:17 -0400

    new version

--- c++/ Merge_k_Sorted_Lists.cpp
+++ c++/ Merge_k_Sorted_Lists.cpp
@@ -7,6 +7,47 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+ /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+       ListNode *dummy = new ListNode(0), *tail = dummy;
+       unordered_map<int, vector<ListNode*>> mp;
+       vector<int> keys;
+       for(int i = 0; i < lists.size(); i++){
+           while(lists[i]){
+               int key = lists[i]->val;
+               if(mp.find(key) != mp.end()){
+                   mp[key].push_back(lists[i]);
+                   lists[i] = lists[i]->next;
+               }
+               else{
+                   mp[key] = {lists[i]};
+                   keys.push_back(key);
+                   lists[i] = lists[i]->next;
+               }
+           }
+       }
+       sort(keys.begin(),keys.end());
+       for(int i = 0; i < keys.size(); i++){
+           for(int j = 0; j < mp[keys[i]].size(); j++){
+               tail->next = mp[keys[i]][j];
+               tail = tail->next;
+           }
+       }
+       return dummy->next;
+    }
+};
+
+
 class Solution {
 public:
         struct compNode {
