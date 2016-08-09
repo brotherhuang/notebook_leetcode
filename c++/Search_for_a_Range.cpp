5da1ef9456ff6fb69b4b72191aeda60c93f8bfba
brotherhuang <jackiehuanghaibin@gmail.com>
2016-08-09 15:03:17 -0400

    add solutions

--- c++/Search_for_a_Range.cpp
+++ c++/Search_for_a_Range.cpp
@@ -9,34 +9,30 @@ Given [5, 7, 7, 8, 8, 10] and target value 8,
 return [3, 4].*/
 class Solution {
 public:
-    int findLeftMost(vector<int>& nums, int target) {
-        int start = 0, end = nums.size()-1;
-        while(start<=end) {
-            int mid = start + (end-start)/2;
-            if(target<nums[mid])
-                end = mid-1;
-            else if(target>nums[mid])
-                start = mid+1;
-            else
-                end = mid-1;
-        }
-        if(start>=0 && start<nums.size() && nums[start]==target) return start;
-        return -1;
-    }
-    int findRightMost(vector<int>& nums, int target) {
-        int start = 0, end = nums.size()-1;
-        while(start<=end) {
-            int mid = start + (end-start)/2;
-            if(target<nums[mid])
-                end = mid-1;
-            else if(target>nums[mid])
-                start = mid+1;
-            else
-                start = mid+1;
-        }
-        if(end>=0 && end<nums.size() && nums[end]==target) return end;
-        return -1;
-    }
+  int findLeftMost(vector<int>& nums, int target) {
+      int start = 0, end = nums.size()-1;
+      while(start<=end) {
+          int mid = start + (end-start)/2;
+          if(target <= nums[mid])
+              end = mid-1;
+          else
+            start = mid+1;
+      }
+      if(start>=0 && start<nums.size() && nums[start]==target) return start;
+      return -1;
+  }
+  int findRightMost(vector<int>& nums, int target) {
+      int start = 0, end = nums.size()-1;
+      while(start<=end) {
+          int mid = start + (end-start)/2;
+          if(target<nums[mid])
+              end = mid-1;
+          else
+              start = mid+1;
+      }
+      if(end>=0 && end<nums.size() && nums[end]==target) return end;
+      return -1;
+  }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> range;
         range.push_back(findLeftMost(nums, target));
