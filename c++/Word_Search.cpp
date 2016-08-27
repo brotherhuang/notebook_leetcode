caa1a4eb8b60d3ad7c8e65d4ba51a4f00c56b755
brotherhuang <jackiehuanghaibin@gmail.com>
2016-08-26 21:14:51 -0400

    modify solution

--- c++/Word_Search.cpp
+++ c++/Word_Search.cpp
@@ -13,7 +13,51 @@ Given board =
 word = "ABCCED", -> returns true,
 word = "SEE", -> returns true,
 word = "ABCB", -> returns false. */
-
+class Solution {
+public:
+    bool dfs(int i, int j, int pos, string word, vector<vector<char>>& board, vector<vector<bool>>& mask){
+        if(pos == word.size() - 1) return true;
+        vector<pair<int,int>> candidate;
+        if(i - 1 >= 0 && !mask[i-1][j]){
+            candidate.push_back(pair<int,int> (i-1,j));
+        }
+        if(i + 1 < board.size()&& !mask[i+1][j]){
+            candidate.push_back(pair<int,int> (i+1,j));
+        }
+        if(j - 1 >= 0&& !mask[i][j-1]){
+            candidate.push_back(pair<int,int> (i,j - 1));
+        }
+        if(j+ 1 < board[0].size()&& !mask[i][j+1]){
+            candidate.push_back(pair<int,int> (i,j + 1));
+        }
+        for(int i = 0; i < candidate.size(); i++){
+            if(board[candidate[i].first][candidate[i].second] == word[pos + 1]){
+                mask[candidate[i].first][candidate[i].second] = true;
+                if(dfs(candidate[i].first, candidate[i].second, pos + 1, word, board, mask)) return true;
+                mask[candidate[i].first][candidate[i].second] = false;
+            }
+        }
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        if(word.size() == 0) {
+            return false;
+        }
+        vector<vector<bool>> mask(board.size(), vector<bool>(board[0].size(), false));
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0; j < board[i].size(); j++) {
+                if(board[i][j] == word[0]) {
+                   mask[i][j] = true;
+                   if(dfs(i, j, 0, word, board, mask)) {
+                       return true;
+                   }
+                   mask[i][j] = false;
+                }
+            }
+        }
+        return false;
+    }
+};
 
 
 class Solution {
