e393b4c354c37ed0d54b9112eb8e00f1146438f7
brotherhuang <jackiehuanghaibin@gmail.com>
2016-09-06 15:45:52 -0400

    add solution

--- c++/Decode_String.cpp
+++ c++/Decode_String.cpp
@@ -14,30 +14,33 @@ s = "2[abc]3[cd]ef", return "abcabccdcdcdef".*/
 class Solution {
 public:
     string decodeString(string s) {
-        string ans, tmp;
-        stack<int> numStack;
-        stack<string> strStack;
-        int n = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (isdigit(s[i]))
-                n = n * 10 + s[i] - '0';
-            else if (s[i] == '[') {
-                numStack.push(n);
-                n = 0;
-                strStack.push(tmp);
-                tmp.clear();
+        string res = "";
+        vector<string> strList;
+        vector<int> repeadSize;
+        string current = "";
+        int nums = 0;
+        string words;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] - '0' >= 0 && s[i] - '0' <= 9) nums = nums * 10 + s[i] - '0';
+            else if(s[i] == '['){
+                repeadSize.push_back(nums);
+                nums = 0;
+                strList.push_back(words);
+                words = "";
             }
-            else if (s[i] == ']') {
-                int k = numStack.top();
-                numStack.pop();
-                for (; k > 0; k--)
-                    strStack.top() += tmp;
-                tmp = strStack.top();
-                strStack.pop();
+            else if(s[i] == ']'){
+                int copyTime = repeadSize.back();
+                repeadSize.pop_back();
+                for(int k = 0; k < copyTime; k++){
+                    strList.back() += words;
+                }
+                words = strList.back();
+                strList.pop_back();
             }
-            else
-                tmp += s[i];
+            else {
+                words.push_back(s[i]);
+            }
         }
-        return strStack.size() == 0 ? tmp : strStack.top();
+        return strList.size() == 0 ? words : strList.back();
     }
 };
