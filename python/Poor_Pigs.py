565799acb99f4dde3ad97ad248af7f71081a7abe
brotherhuang <jackiehuanghaibin@gmail.com>
2016-11-09 22:00:36 -0500

    add solution

---
+++ python/Poor_Pigs.py
@@ -0,0 +1,25 @@
+"""
+There are 1000 buckets, one and only one of them contains poison, the rest are filled with water. They all look the same. If a pig drinks that poison it will die within 15 minutes. What is the minimum amount of pigs you need to figure out which bucket contains the poison within one hour.
+
+Answer this question, and write an algorithm for the follow-up general case.
+
+Follow-up:
+
+If there are n buckets and a pig drinking poison will die within m minutes, how many pigs (x) you need to figure out the "poison" bucket within p minutes? There is exact one bucket with poison.
+"""
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
+        if buckets == 1 : return 0
+        base = (minutesToTest - 1)/minutesToDie + 2
+        res = 0
+        buckets = buckets - 1
+        while buckets > 0:
+            buckets /= base
+            res += 1
+        return res
